22f3dafd707d854ca86a8ecc47dd822863d6741a
Ezekiel Gray <ezekiel.gray@zoho.com>
2022-05-07 19:58:28 -0400

    add

--- myApp.js
+++ myApp.js
@@ -91,10 +91,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  Person.remove({ name: nameToRemove }, (err, response) => {
+  Person.remove({name: nameToRemove}, (err, response) => {
     if(err) return console.log(err);
     done(null, response);
-  })
+  });
 };
 
 const queryChain = (done) => {
