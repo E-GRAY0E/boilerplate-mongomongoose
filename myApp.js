80d87d1eb057af1158277554699a0ce9f8b9a8f3
Ezekiel Gray <ezekiel.gray@zoho.com>
2022-05-07 21:26:55 -0400

    add

--- myApp.js
+++ myApp.js
@@ -99,8 +99,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person
+    .find({ favoriteFood: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0})
+    .exec((err, people)=>{
+      done(err, people);
+    });  
 };
 
 /** **Well Done !!**
